5d47c085424b41708e21f378b275e373657c8360
Osekobo <collinsboseko2005@gmail.com>
2026-02-20 18:00:02 +0300

    Re-learning flask_api

--- models.py
+++ models.py
@@ -1,6 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
 db = SQLAlchemy()
 
 
@@ -10,78 +9,14 @@ class Product(db.Model):
     name = db.Column(db.String(256), nullable=False)
     buying_price = db.Column(db.Float, nullable=False)
     selling_price = db.Column(db.Float, nullable=False)
-    model = db.Column(db.String, nullable=False)
-    year = db.Column(db.Integer, nullable=False)
-    condition = db.Column(db.String, nullable=False)
-    fuel = db.Column(db.String, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(
-        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
 
-class Payment(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    mode = db.Column(db.String(80), nullable=False)
-    sale_id = db.Column(db.Integer, db.ForeignKey('sales.id'), nullable=False)
-    mpesa_ref = db.Column(db.String(120), nullable=False)
-    trans_amount = db.Column(db.Integer, nullable=False)
-    trans_name = db.Column(db.String(120), nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.now)
-
-    sale = db.relationship('Sale', backref=db.backref('payments', lazy=True))
-
-
-class Sale(db.Model):
-    __tablename__ = "sales"
-    id = db.Column(db.Integer, primary_key=True)
-    # quantity = db.Column(db.Float, nullable=False)
-    # product_id = db.Column(db.Integer, db.ForeignKey(
-    #      "products.id"), nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(
-        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
-    # product = db.relationship(
-    #     "Product", backref=db.backref("sales", lazy=True))
-    details = db.relationship("SalesDetails", backref="sale", lazy=True)
-
-
-class SalesDetails(db.Model):
-    __tablename__ = 'sales_details'
-    id = db.Column(db.Integer, primary_key=True)
-    sale_id = db.Column(db.Integer, db.ForeignKey('sales.id'), nullable=False)
-    product_id = db.Column(db.Integer, nullable=False)
-    quantity = db.Column(db.Integer, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.now)
-
-
-class Purchase(db.Model):
+class Purchases(db.Model):
     __tablename__ = "purchases"
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
     quantity = db.Column(db.Float, nullable=False)
     product_id = db.Column(db.Integer, db.ForeignKey(
         "products.id"), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(
         db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
-    product = db.relationship(
-        "Product", backref=db.backref("purchases", lazy=True))
-
-
-class User(db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(256), nullable=False)
-    phone = db.Column(db.String(256), unique=True, nullable=False)
-    email = db.Column(db.String(256), unique=True, nullable=False)
-    password = db.Column(db.String(256), nullable=False)  # hashed
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(
-        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
-
-
-class OTP(db.Model):
-    __tablename__ = "otps"
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    otp = db.Column(db.String(6), nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
