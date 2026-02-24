1fc2385b368fc81b12c711ef9fdde6ed6dba5513
Osekobo <collinsboseko2005@gmail.com>
2026-02-24 16:20:41 +0300

    Register route

--- main.py
+++ main.py
@@ -1,60 +1,118 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request, jsonify
+from models import db, Product, Purchase, datetime, User
 from flask_cors import CORS
-from models import db, Products, Purchases
+from collections import defaultdict
+from werkzeug.security import generate_password_hash
+from flask_jwt_extended import JWTManager,create_access_token
 app = Flask(__name__)
-CORS(app)
 
 app.config['JWT_SECRET_KEY'] = 'hgyutd576uyfutu'
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:12039@localhost:5432/learning"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
+CORS(app)
 
+jwt = JWTManager(app)
 
 @app.route("/")
 def home():
-    return ("Flask version 1.0")
+    return ("Flask version: 1.0"), 200
+
+
+@app.route("/register", methods=["POST"])
+def register():
+    data = dict(request.get_json())
+    if "name"not in data.keys() or "phone"not in data.keys() or "email"not in data.keys() or "password"not in data.keys():
+        error = {"error": "Ensure all fields are set!"}
+        return jsonify(error), 400
+    elif User.query.filter_by(email=data["email"]).first() is not None:
+        error = {"error": "User with email already exists!"}
+        return jsonify(error), 409
+    else:
+        usr = User(name=data["name"], phone=data["phone"],
+                   email=data["email"], password=generate_password_hash(data["password"]))
+        db.session.add(usr)
+        db.session.commit()
+        data["id"] = usr.id
+        token = create_access_token(data["email"])
+        message = {"message": "User registered successfully!"}
+        return jsonify(message, {"token": token}), 201
 
 
 @app.route("/products", methods=["GET", "POST"])
 def products():
     if request.method == "GET":
-        product = Products.query.all()
+        product = Product.query.all()
         product_list = []
         for x in product:
             data = {"id": x.id, "name": x.name,
                     "buying_price": x.buying_price, "selling_price": x.selling_price}
             product_list.append(data)
-        return jsonify(product_list)
+            return jsonify(product_list), 200
     elif request.method == "POST":
         data = dict(request.get_json())
-        if "name"not in data.keys() or "buying_price"not in data.keys() or "selling_price"not in data.keys():
-            error = {"error": "Ensure all fields are set"}
+        if "name" not in data.keys() or "buying_price"not in data.keys() or "selling_price"not in data.keys():
+            error = {"error": "Ensure all fields are set!"}
             return jsonify(error), 400
         else:
-            prod = Products(
-                name=data["name"], buying_price=data["buying_price"], selling_price=data["selling_price"])
+            prod = Product(
+                name=data["name"], selling_price=data["selling_price"], buying_price=data["buying_price"])
             db.session.add(prod)
             db.session.commit()
             data["id"] = prod.id
             return jsonify(data), 201
     else:
-        error = {"error": "Method not allowed"}
+        error = {"error": "Method not allowed!"}
         return jsonify(error), 405
 
 
-@app.route("/purchases")
+@app.route("/purchases", methods=["GET", "POST"])
 def purchases():
-    purchases = Purchases.query.all()
-    purchases_list = []
-    for x in purchases:
-        data = {"id": x.id, "quantity": x.quantity, "product_id": x.product_id,
-                "created_at": x.created_at.strftime("%Y-%m-%d %H:%M:%S"), "updated_at": x.updated_at.strftime("%Y-%m-%d %H:%M:%S")}
-        purchases_list.append(data)
-        return jsonify(purchases_list)
-    # return ("Purchase page")
+    if request.method == "GET":
+        purchase = Purchase.query.all()
+        purchase_list = []
+        for p in purchase:
+            data = {"quantity": p.quantity, "product_id": p.product_id,
+                    "created_on": p.created_on, "updated_on": p.updated_on}
+            purchase_list.append(data)
+            return jsonify(purchase_list), 200
+    elif request.method == "POST":
+        data = dict(request.get_json())
+        if "created_on"not in data.keys():
+            data["created_on"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            pass
+        if "quantity" not in data.keys() or "product_id"not in data.keys():
+            error = {"error": "Ensure all fields are set!"}
+            return jsonify(error), 400
+        else:
+            data = Purchase(
+                product_id=data["product_id"], quantity=data["quantity"])
+            db.session.add(data)
+            db.session.commit()
+            data["id"] = data.id
+            data["updated_on"] = data.updated_on.strftime("%Y-%m-%d %H:%M:%S")
+            return jsonify(data), 201
+    else:
+        error = {"error": "Method not allowed!"}
+        return jsonify(error), 405
+    return ("")
+
+
+@app.route("/sales", methods=["GET", "POST"])
+def sales():
+    if request.method == "GET":
+        results = ()
+        sales_list = defaultdict(list)
+        pass
+    elif request.method == "POST":
+        pass
+    else:
+        error = {"error": "Method not allowed!"}
+        return jsonify(error), 405
 
 
 if __name__ == "__main__":
     with app.app_context():
         # db.drop_all()
         db.create_all()
-app.run()
+    app.run()
--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from datetime import datetime
 db = SQLAlchemy()
 
 
-class Products(db.Model):
+class Product(db.Model):
     __tablename__ = "products"
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     name = db.Column(db.String(256), nullable=False)
@@ -11,12 +11,41 @@ class Products(db.Model):
     selling_price = db.Column(db.Float, nullable=False)
 
 
-class Purchases(db.Model):
+class Purchase(db.Model):
     __tablename__ = "purchases"
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     quantity = db.Column(db.Float, nullable=False)
-    product_id = db.Column(
-        db.Integer, db.ForeignKey("products.id"), nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(
+    product_id = db.Column(db.Integer, db.ForeignKey(
+        "products.id"), nullable=False)
+    created_on = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_on = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+
+class Sale(db.Model):
+    __tablename__ = "sales"
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    created_on = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_on = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+
+class SaleDetails(db.Model):
+    __tablename__ = "sales_details"
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    sale_id = db.Column(db.Integer, db.ForeignKey("sales.id"), nullable=False)
+    product_id = db.Column(db.Integer, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    created_on = db.Column(db.DateTime, default=datetime.now)
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String(256), nullable=False)
+    phone = db.Column(db.String(256), nullable=False)
+    email = db.Column(db.String(256), nullable=False)
+    password = db.Column(db.String(256), nullable=False)
+    created_on = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_on = db.Column(
         db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
