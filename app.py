42ede603022d1182cc47d03dba9f1aa4ca51600f
Osekobo <collinsboseko2005@gmail.com>
2026-02-27 19:55:57 +0300

    python tasks in try.py

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 from flask import Flask, request, jsonify
+from models import db, User, Product, Purchase, Sale
 from flask_jwt_extended import create_access_token
-from werkzeug.security import generate_password_hash, check_password_hash
-from models import db, User
+from werkzeug.security import check_password_hash
+from collections import defaultdict
 app = Flask(__name__)
 
 
-@app.route("register", method=["POST"])
+@app.route("/")
+def home():
+    return ("Flask version 1.0")
+
+
+@app.route("/register", methods=["POST"])
 def register():
     data = request.get_json()
     if not data:
-        return jsonify({"error": "Invalid JSON data!"}), 400
+        return jsonify({"error": "Invalid JSON format"}), 400
 
     if not all(k in data for k in ("name", "phone", "email", "password")):
         return jsonify({"error": "Ensure all fields are set!"}), 400
@@ -18,43 +24,108 @@ def register():
 
     if User.query.filter_by(email=email).first():
         return jsonify({"error": "Email already exists!"}), 409
-
     usr = User(name=data["name"], phone=data["phone"],
-               email=email, password=generate_password_hash(data["password"]))
+               email=email, password=data["password"])
 
     db.session.add(usr)
     db.session.commit()
     token = create_access_token(identity=usr.id)
-    return jsonify({"message": "success", "Token": token}), 201
+    return jsonify({"message": "User registered!", "token": token}), 201
 
 
 @app.route("/login", methods=["POST"])
 def login():
     data = request.get_json()
-
     if not data:
-        return jsonify({"error": "Invalid JSON data!"}), 400
+        return jsonify({"error": "Invalid JSON format"}), 400
 
-    email = data.get(email=email).lower().strip()
+    if "email"not in data or "password"not in data:
+        return jsonify({"error": "Ensure all fields are set!"}), 400
+
+    email = data.get("email").lower().strip()
     password = data.get("password")
 
     usr = User.query.filter_by(email=email).first()
 
-    email = email
-    password = data.get["password"]
-
-    if not usr or check_password_hash(usr.password, password):
-        return jsonify({"error": "Invalid email or password"}), 401
+    if not usr or not check_password_hash(usr.password, password):
+        return jsonify({"error": "Invalid email or password!"})
 
     token = create_access_token(identity=usr.id)
-    return jsonify({"message": "success", "Token": token}), 200
-    pass
+
+    return jsonify({"token": token}), 200
 
 
 @app.route("/products", methods=["GET", "POST"])
-def register():
+def products():
     if request.method == "GET":
-        pass
+        products = Product.query.all()
+        product_list = []
+        for x in products:
+            data = {}
+            product_list.append(data)
+        return jsonify(product_list), 200
+    elif request.method == "POST":
+        data = request.get_json()
+
+        if not data:
+            return jsonify({"error": "Invalid JSON body"}), 400
+
+        name = data.get("name")
+        buying_price = data.get("buying_price")
+        selling_price = data.get("selling_price")
+
+        if not name or buying_price is None or selling_price is None:
+            return jsonify({"error": "Ensure all fields are set!"}), 400
+
+        if selling_price < 0 or buying_price < 0:
+            return jsonify({"error": "Ensure prices are above 0"}), 400
+
+        prod = Product(name=name,
+                       buying_price=buying_price,
+                       selling_price=selling_price)
+        db.session.add(prod)
+        db.session.commit()
+        return jsonify({"id": prod.id}), 201
+
+    return jsonify({"error": "Method not allowed!"}), 405
+
+
+@app.route("/purchases", methods=["GET", "POST"])
+def purchases():
+    if request.method == "GET":
+        purchases = Purchase.query.all()
+        purchase_list = []
+        for p in purchases:
+            data = {}
+            purchase_list.append(data)
+        return jsonify(purchase_list), 200
+    elif request.method == "POST":
+        data = request.get_json()
+        if not data:
+            return jsonify({"error": "Invalid JSON data!"}), 400
+        if "quantity"not in data or "product_id"not in data:
+            return jsonify({"error": "Ensure all fields are set!"}), 400
+        purch = Purchase()
+        db.session.add(purch)
+        db.session.commit()
+        response = {}
+        return jsonify(response), 201
+    return jsonify({"error": "Method not allowed!"}), 400
+
+
+@app.route("/sales", methods=["GET", "POST"])
+def sales():
+    if request.method == "GET":
+        data = ()
+        sales_list = defaultdict(list)
+        for sale, detail, product in data:
+            sales_list[sale.id].append((sale, detail, product))
+        result = []
+        return jsonify(result), 200
     elif request.method == "POST":
         pass
-    return jsonify({"error": "Method not allowed!"})
+    return jsonify({"error": "Method not allowed!"}), 400
+
+@app.route("/forgot_password",methods=["POST"])
+def forgot_password():
+    pass
---
+++ try.py
@@ -0,0 +1,85 @@
+
+
+def area():
+    b = int(input("Enter the base: "))
+    h = int(input("Enter the height: "))
+    a = 1/2*b*h
+    print(a)
+
+
+# area()
+
+def number():
+    n = int(input("Enter number: "))
+    if n > 0:
+        if n % 2 == 0:
+            if n % 4 == 0:
+                print("multiple of 4")
+            else:
+                print("even")
+        else:
+            print("odd")
+    else:
+        print("number is less than 0")
+
+
+# number()
+
+def phone():
+    n = input("Enter phone number: ")
+    if n.startswith("+254"):
+        print(n)
+    elif n.startswith("07"):
+        print("+254"+n[1:])
+    elif n.startswith("01"):
+        print("+254"+n[1:])
+    elif n.startswith("7"):
+        print("+254"+n)
+    elif n.startswith("254"):
+        print("+"+n)
+    else:
+        print("Invalid number")
+
+
+# phone()
+
+def email():
+    e = str(input("Enter email: "))
+    x = e.count("@" and ".")
+    if x > 0:
+        print("Valid email")
+    else:
+        print("Invalid email")
+
+
+# email()
+
+def number():
+    a = int(input("Enter number: "))
+    b = int(input("Enter number: "))
+    c = int(input("Enter number: "))
+    if a > b and a > c:
+        print("A is the largest")
+    elif b > a and b > c:
+        print("B is the largest")
+    elif c > a and c > b:
+        print("C is the largest")
+    else:
+        print("Invalid number")
+
+
+# number()
+
+def password():
+    tries = 4
+    pas = "admin@123"
+    while tries > 0:
+        p = input("password: ")
+        if p == pas:
+            print("Granted")
+            break
+        else:
+            tries = tries-1
+            print("Wrong", tries)
+    if tries == 0:
+        print("Blocked")
