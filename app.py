5dcf80ba3d13f663c55c0ab00d9304525c093103
Osekobo <collinsboseko2005@gmail.com>
2026-02-28 23:17:27 +0300

    Re_learning flask_api

--- app.py
+++ app.py
@@ -1,58 +1,40 @@
 from flask import Flask, request, jsonify
-from models import db, User, Product, Purchase, Sale
+from models import db, User, Product, Purchase
 from flask_jwt_extended import create_access_token
-from werkzeug.security import check_password_hash
-from collections import defaultdict
+from werkzeug.security import generate_password_hash
 app = Flask(__name__)
-
-
-@app.route("/")
-def home():
-    return ("Flask version 1.0")
 
 
 @app.route("/register", methods=["POST"])
 def register():
     data = request.get_json()
     if not data:
-        return jsonify({"error": "Invalid JSON format"}), 400
-
+        return jsonify({"error": "Invalid JSON data"}), 400
     if not all(k in data for k in ("name", "phone", "email", "password")):
-        return jsonify({"error": "Ensure all fields are set!"}), 400
-
+        return jsonify({"error": "Ensure all fields are set"}), 400
     email = data["email"].lower().strip()
-
     if User.query.filter_by(email=email).first():
-        return jsonify({"error": "Email already exists!"}), 409
+        return jsonify({"error": "Email already exists"}), 409
     usr = User(name=data["name"], phone=data["phone"],
                email=email, password=data["password"])
-
     db.session.add(usr)
     db.session.commit()
     token = create_access_token(identity=usr.id)
-    return jsonify({"message": "User registered!", "token": token}), 201
+    return jsonify({"message": "User created", "Token": token}), 201
 
 
 @app.route("/login", methods=["POST"])
 def login():
     data = request.get_json()
-    if not data:
-        return jsonify({"error": "Invalid JSON format"}), 400
-
-    if "email"not in data or "password"not in data:
+    if not data or "email" not in data or "password" not in data:
         return jsonify({"error": "Ensure all fields are set!"}), 400
-
     email = data.get("email").lower().strip()
     password = data.get("password")
-
     usr = User.query.filter_by(email=email).first()
-
-    if not usr or not check_password_hash(usr.password, password):
-        return jsonify({"error": "Invalid email or password!"})
-
+    if not email or not generate_password_hash(usr.password, password):
+        return jsonify({"error": "Invalid email or password"}), 401
     token = create_access_token(identity=usr.id)
-
-    return jsonify({"token": token}), 200
+    return jsonify({"message": "Login successful", "Token": token})
 
 
 @app.route("/products", methods=["GET", "POST"])
@@ -60,33 +42,25 @@ def products():
     if request.method == "GET":
         products = Product.query.all()
         product_list = []
-        for x in products:
+        for p in products:
             data = {}
             product_list.append(data)
-        return jsonify(product_list), 200
+        return jsonify(product_list)
     elif request.method == "POST":
         data = request.get_json()
-
         if not data:
-            return jsonify({"error": "Invalid JSON body"}), 400
-
+            return jsonify({"error": "Invalid JSON data!"}), 400
         name = data.get("name")
         buying_price = data.get("buying_price")
         selling_price = data.get("selling_price")
-
         if not name or buying_price is None or selling_price is None:
             return jsonify({"error": "Ensure all fields are set!"}), 400
-
-        if selling_price < 0 or buying_price < 0:
+        if buying_price < 0 or selling_price < 0:
             return jsonify({"error": "Ensure prices are above 0"}), 400
-
-        prod = Product(name=name,
-                       buying_price=buying_price,
-                       selling_price=selling_price)
+        prod = Product()
         db.session.add(prod)
         db.session.commit()
         return jsonify({"id": prod.id}), 201
-
     return jsonify({"error": "Method not allowed!"}), 405
 
 
@@ -94,38 +68,11 @@ def products():
 def purchases():
     if request.method == "GET":
         purchases = Purchase.query.all()
-        purchase_list = []
+        purchases_list = []
         for p in purchases:
             data = {}
-            purchase_list.append(data)
-        return jsonify(purchase_list), 200
-    elif request.method == "POST":
-        data = request.get_json()
-        if not data:
-            return jsonify({"error": "Invalid JSON data!"}), 400
-        if "quantity"not in data or "product_id"not in data:
-            return jsonify({"error": "Ensure all fields are set!"}), 400
-        purch = Purchase()
-        db.session.add(purch)
-        db.session.commit()
-        response = {}
-        return jsonify(response), 201
-    return jsonify({"error": "Method not allowed!"}), 400
-
-
-@app.route("/sales", methods=["GET", "POST"])
-def sales():
-    if request.method == "GET":
-        data = ()
-        sales_list = defaultdict(list)
-        for sale, detail, product in data:
-            sales_list[sale.id].append((sale, detail, product))
-        result = []
-        return jsonify(result), 200
+            purchases_list.append(data)
+        return jsonify(purchases_list), 200
     elif request.method == "POST":
         pass
-    return jsonify({"error": "Method not allowed!"}), 400
-
-@app.route("/forgot_password",methods=["POST"])
-def forgot_password():
-    pass
+    return jsonify({"error": "Method not allowed!"}), 405
